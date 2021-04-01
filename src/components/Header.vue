508df383734aa556cdb69253ce9bcbca9ea13ea5
Leila Sosa González <leisosa93@gmail.com>
2021-04-01 16:31:30 -0300

    primer commit

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="active item">Image Storage</a>
+    <div class="right menu">
+      <a href="/" class="ui item">Log In</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style></style>
